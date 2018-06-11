4adc4872b69c8fd2d23d5f5e33ff83769df6dc48
JoJo Webster <40183252+makutamiserix@users.noreply.github.com>
2018-06-11 16:27:26 -0500

    Add project files.

---
+++ Webster.ManagedServices.Contracts/IRandomNumberService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webster.ManagedServices.Contracts
+{
+    [ServiceContract]
+    public interface IRandomNumberService
+    {
+        [OperationContract] byte[] GetRandomBytes(int size);
+        [OperationContract] string GetRandomInteger(byte[] value);
+    }
+}
