0492ad34956206bc6f1f06ad3007a84e7423f7d8
JoJo Webster <40183252+makutamiserix@users.noreply.github.com>
2018-06-11 17:45:12 -0500

    0

---
+++ Webster.ManagedServices.Contracts/IStreamingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webster.ManagedServices.Contracts
+{
+    public interface IStreamingService
+    {
+        [OperationContract] Stream GetAudioStream();
+        [OperationContract] Stream[] GetAudioStreams(Guid playListID);
+    }
+}
---
+++ Webster.ManagedServices.Data/DataManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webster.ManagedServices.Data
+{
+    public class DataManager : System.ComponentModel.Component
+    {
+        private MediaDataSetTableAdapters.QueriesTableAdapter queriesAdapter;
+        private MediaDataSetTableAdapters.SelectPlaylistTableAdapter selectPlaylistAdapter;
+
+        private void InitializeComponent()
+        {
+            this.queriesAdapter = new Webster.ManagedServices.Data.MediaDataSetTableAdapters.QueriesTableAdapter();
+            this.selectPlaylistAdapter = new Webster.ManagedServices.Data.MediaDataSetTableAdapters.SelectPlaylistTableAdapter();
+            // 
+            // selectPlaylistAdapter
+            // 
+            this.selectPlaylistAdapter.ClearBeforeFill = true;
+
+        }
+
+        public DataManager() : base()
+        {
+            this.InitializeComponent();
+        }
+
+
+    }
+}
