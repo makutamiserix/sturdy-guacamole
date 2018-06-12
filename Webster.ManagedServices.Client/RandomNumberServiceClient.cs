849c654a60aa23836ce5c7dce0d7634058730afc
JoJo Webster <40183252+makutamiserix@users.noreply.github.com>
2018-06-12 15:29:10 -0500

    2

---
+++ Webster.ManagedServices.Client/RandomNumberServiceClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Webster.ManagedServices.Contracts;
+
+namespace Webster.ManagedServices.Client
+{
+    public class RandomNumberServiceClient : ClientBase<IRandomNumberService>, IRandomNumberService
+    {
+        public byte[] GetRandomBytes(int size)
+        {
+            return this.Channel.GetRandomBytes(size);
+        }
+
+        public string GetRandomInteger(byte[] value)
+        {
+            return this.Channel.GetRandomInteger(value);
+        }
+    }
+}
---
+++ Webster.ManagedServices.Client/StreamingServiceClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Webster.ManagedServices.Contracts;
+
+namespace Webster.ManagedServices.Client
+{
+    public class StreamingServiceClient : ClientBase<IStreamingService>, IStreamingService
+    {
+        public Stream GetAudioStream(Guid songID)
+        {
+            return this.Channel.GetAudioStream(songID);
+        }
+
+        public Stream[] GetAudioStreams(Guid playListID)
+        {
+            return this.Channel.GetAudioStreams(playListID);
+        }
+    }
+}
